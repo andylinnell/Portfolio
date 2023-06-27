dd196d88ef588c92b91a7d635c518c1de7aae8c9
Andy Linnell <andylinnell.cafo@gmail.com>
2023-06-27 13:21:47 -0400

    Added AI2023 cert

--- src/components/Certs.js
+++ src/components/Certs.js
@@ -11,6 +11,7 @@ import adobeIllQS from "../assets/certs/AdobeIllCert-pdf.webp"
 import css from "../assets/certs/cssgraphicsCert-pdf.webp"
 import photoshop1 from "../assets/certs/photoshopcert-pdf.webp"
 import photoshop2 from "../assets/certs/quickphotoshopCert-pdf.webp"
+import adobeillu from "../assets/certs/adobeillu.webp"
 
 
 
@@ -23,6 +24,8 @@ export default function Certs() {
             {/* <div className="flex flex-col md:flex-row flex-wrap mt-4 gap-5"> */}
             <div className="grid grid-cols-1 md:grid-cols-3 justify-center mt-4 gap-5">
                 <CertCard name="Software Engineering Career Course" img={bc} issued="Boca Code School" date="Mar 2023" />
+                <CertCard name="Adobe Illustrator 2023 Essential Training" img={adobeillu} issued="LinkedIn Learning" date="June 2023" />
+
                 <CertCard name="UX DesignOps: Managing Stakeholders" img={UXCert} issued="LinkedIn Learning" date="Apr 2023" />
                 <CertCard name="Adobe Illustrator 2023" img={adobeIllQS} issued="LinkedIn Learning" date="June 2023" />
                 <CertCard name="Photoshop 2021 Start" img={photoshop2} issued="LinkedIn Learning" date="June 2023" />
